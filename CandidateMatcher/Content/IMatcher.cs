5ccb216632e0578761ae4c82140c4706da573d00
hph138 <hph138@hotmail.com>
2021-01-17 23:38:54 +1100

    Added match function.

---
+++ CandidateMatcher/Content/IMatcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateMatcher.Content
+{
+     public interface IMatcher
+    {
+         int CountMatches(string source, string target);
+    }
+}
---
+++ CandidateMatcher/Content/Matcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateMatcher.Content
+{
+    public class Matcher : IMatcher
+    {
+        public int CountMatches(string source, string target)
+        {
+            IEnumerable<string> src = GetDistinctList(source);
+            IEnumerable<string> tar = GetDistinctList(target);
+            return src.Count(w => tar.Contains(w));
+        }
+
+        private static IEnumerable<string> GetDistinctList(string source)
+        {
+            return source.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).Where(s => s != string.Empty).Distinct();
+        }
+    }
+}
